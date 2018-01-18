ffeb1961fc9b47e5197bdbdb5385b159a16102f1
Matthew Haskett <itztheendmc@gmail.com>
2018-01-18 18:14:38 +0000

    Give me xp for testing

--- src/main/java/me/enderaura/opex/misc/LoginHandler.java
+++ src/main/java/me/enderaura/opex/misc/LoginHandler.java
@@ -1,8 +1,10 @@
 package me.enderaura.opex.misc;
 
+import me.enderaura.opex.leveling.LevelingData;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
 
+import java.util.Random;
 import java.util.Timer;
 
 /**
@@ -14,6 +16,10 @@ public class LoginHandler {
     @EventSubscriber
     public void onLogin(ReadyEvent event){
         event.getClient().changeUsername("Opex");
+
+
+        LevelingData.getInstance().setXpForUser(event.getClient().getUserByID(178806549710503936L), 99);
+
 
         Timer timer = new Timer();
         timer.schedule(new StatusHandler(), 0, 10000);
